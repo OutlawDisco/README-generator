c5d89f06f4668b65ab81274d4338837cca599e7c
OutlawDisco <aoutlaw00@gmail.com>
2023-06-10 10:52:54 -0600

    adding prompts for read me file construction

--- index.js
+++ index.js
@@ -1,57 +1,91 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// const generateMD = ({ name, email, projectName, description, license }) =>
-// `# <Your-Project-Title>
+const generateMD = ({ name, email, projectName, description, license }) =>
+  `# ${projectName}
+By ${name}
+email:${email}
+## Description
 
-// ## Description
+${description} Use the following questions as a guide:
 
-// Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
 
-// - What was your motivation?
-// - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-// - What problem does it solve?
-// - What did you learn?
+## Table of Contents (Optional)
 
-// ## Table of Contents (Optional)
+If your README is long, add a table of contents to make it easy for users to find what they need.
 
-// If your README is long, add a table of contents to make it easy for users to find what they need.
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
 
-// - [Installation](#installation)
-// - [Usage](#usage)
-// - [Credits](#credits)
-// - [License](#license)
+## Installation
 
-// ## Installation
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 
-// What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+## Usage
 
-// ## Usage
+Provide instructions and examples for use. Include screenshots as needed.
 
-// Provide instructions and examples for use. Include screenshots as needed.
 
-// To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+add a screenshot
 
-// ![alt text](assets/images/screenshot.png)
+## Credits
 
-// ## Credits
+List your collaborators, if any, with links to their GitHub profiles.
 
-// List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 
-// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well.
 
-// If you followed tutorials, include links to those here as well.
+## ${license}
 
-// ## License
 
-// The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+---
 
-// ---
+## Features
 
-// ## Features
+If your project has a lot of features, list them here.
 
-// If your project has a lot of features, list them here.
+## How to Contribute
 
-// ## How to Contribute
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.`;
 
-// If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.`
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your username?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "project name?",
+      name: "projectName",
+    },
+    {
+      type: "input",
+      message: "Please write a short description of your project",
+      name: "description",
+    },
+    {
+      type: "input",
+      chioces: "MIT",
+    },
+  ])
+  .then((answers) => {
+    const mdPageContent = generateMD(answers);
+
+    fs.writeFile("README.md", mdPageContent, (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+  });
